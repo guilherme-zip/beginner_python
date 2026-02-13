99220c2b96b961ee332cd8b83343b0d2bfc61a2f
Guilherme <souto.guilhermectt@outlook.com>
2026-02-13 13:14:00 -0300

    13 Feb.2026

--- Curso_santander_intropython/level2/c_set.py
+++ Curso_santander_intropython/level2/c_set.py
@@ -2,8 +2,6 @@
 Conjunto é uma estrutura de dados mutáveis e não ordenados que permite armazenar coleção de documentos únicos ,são definidos por { } ou criados com a função set()
 """
 frutas ={"apple","pineple","banana","green apple"}
-numeros = set([1,2,3,4])
-
 frutas.add("pera") 
 print(",".join(frutas)) #Saída {"apple","pineple","banana","green apple","pera"}
 #",".join -->Faz com que o código retorne sem as chaves e áspas 
